6f1b261b49936609b6b980e905b54112a8cbf03f
Tmbao <tmbao@apcs.vn>
2015-05-20 00:33:30 +0700

    Update inverted_index

--- utils/inverted_index.h
+++ utils/inverted_index.h
@@ -14,10 +14,10 @@ struct inverted_index {
     vector<double> sum_frequency;
     vector<vector<double>> tfidf;
 
+    bool is_build;
 
-
-    inverted_index() {}
-    inverted_index(int n): index(n), frequency(n), tfidf(n), n_words(n), n_docs(0) {}
+    inverted_index(): is_build(false) {}
+    inverted_index(int n): index(n), frequency(n), tfidf(n), n_words(n), n_docs(0), is_build(false) {}
 
     void build_tfidf() {
         for (int i = 0; i < n_words; i++) 
@@ -32,19 +32,50 @@ struct inverted_index {
             double idf = log(double(n_docs) / index[i].size());
 
             for (int j = 0; j < index[i].size(); j++) {
-                double tf = frequency[i][j] / sum_frequency[index[i][j]];
+                double tf = sqrt(frequency[i][j] / sum_frequency[index[i][j]]);
 
                 tfidf[i][j] = tf * idf;
             }
         }
+
+        is_build = true;
+    }
+
+    vector<double> make_query_tfidf(vec freq, uvec term_id) {
+
+        if (!is_build) 
+            throw logic_error("You must perform build_tfidf before make_query_tfidf");
+
+        vector<double> q_tfidf(n_words);
+
+        vector<double> q_frequency(n_words, 0);
+        double q_sum_frequency = 0;
+        for (int i = 0; i < term_id.n_elem; i++) {
+            q_frequency[term_id[i]] += freq[i];
+            q_sum_frequency += freq[i];
+        }
+
+        for (int i = 0; i < n_words; i++) {
+            double idf = log(double(n_docs) / index[i].size());
+            double tf = sqrt(q_frequency[i] / q_sum_frequency);
+
+            q_tfidf[i] = tf * idf;
+        }
+
+        return q_tfidf;
     }
 
     void add(vec freq, uvec term_id, int doc_id) {
         n_docs++;
         for (int i = 0; i < term_id.n_elem; i++) {
-            index[term_id[i]].push_back(doc_id);
-            frequency[term_id[i]].push_back(freq[i]);
+            if (index[term_id[i]].empty() || index[term_id[i]].back() != doc_id) {
+                index[term_id[i]].push_back(doc_id);
+                frequency[term_id[i]].push_back(0);
+            }
+            frequency[term_id[i]].back() += freq[i];
         }
+
+        is_build = false;
     }
 };
 
