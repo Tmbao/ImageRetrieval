3e1d2e0c67f37ed7dfe1ca50b7aea233004dffe0
root <root@vmw192168024066.uit.edu.vn>
2015-05-24 22:59:41 +0700

    Update .gitignore

---
+++ CMakeFiles/ImageRetrievalCpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ImageRetrievalCpp.dir/main.cpp.o"
+  "ImageRetrievalCpp.pdb"
+  "ImageRetrievalCpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ImageRetrievalCpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
