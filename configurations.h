75387c1062161f83b9e8d1cf5892a44eacde7d08
Thuyen Phan <pvthuyen@apcs.vn>
2015-05-26 20:57:23 +0700

    Edit configurations.h

--- configurations.h
+++ configurations.h
@@ -23,7 +23,7 @@ const double EPS = 1e-9;
 const string featureConfig = "-hesaff -sift -noangle";
 const string computeDescriptorPath = "./feature_detector/compute_descriptors_64bit.ln";
 
-const string codebookFile = "data/oxbuilding_codebook_l2_1000000_24464227_128_50it_new.hdf5";
+const string codebookFile = "data/oxbuilding_codebook_l2_1000000_24464227_128_50it.hdf5";
 const string indexFile = "data/flannIndex.hdf5";
 
 const string dataFolder = "data/images";
