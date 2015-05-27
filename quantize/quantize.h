4033c413f18363e66782504ac4beed7d0ebbbf8e
Thuyen Phan <pvthuyen@apcs.vn>
2015-05-27 21:54:52 +0700

    Edit quantize.h

--- quantize/quantize.h
+++ quantize/quantize.h
@@ -10,16 +10,23 @@ const int nChecks = 800;
 const int dataKnn = 1;
 const int queryKnn = 3;
 
+cvflann::Matrix<double> dataset;
+cvflann::Index<cvflann::L2<double>> *treeIndex;
+
 void buildIndex() {
     cvflann::Matrix<double> dataset;
 
     cvflann::load_from_file(dataset, codebookFile, "clusters");
 
-    if (boost::filesystem::exists(indexFile))
-        return;
+    cvflann::IndexParams *indexParams;
 
-    cvflann::Index<cvflann::L2<double> > treeIndex(dataset, cvflann::KDTreeIndexParams(nKdTree));
-    treeIndex.save(indexFile);
+    if (boost::filesystem::exists(indexFile))
+        indexParams = new cvflann::SavedIndexParams(indexFile);
+    else
+        indexParams = new cvflann::KDTreeIndexParams(nKdTree);
+
+    treeIndex = new cvflann::Index<cvflann::L2<double> > (dataset, *indexParams);
+    treeIndex->buildIndex();
 }
 
 void buildBoW(const mat &imageDesc, mat &weights, umat &termID, const string &weightPath, const string &termIDPath) {
@@ -33,18 +40,10 @@ void buildBoW(const mat &imageDesc, mat &weights, umat &termID, const string &we
     memcpy(tmpData, imageDesc.memptr(), sizeof(double) * imageDesc.n_elem);
     cvflann::Matrix<double> query(tmpData, imageDesc.n_cols, imageDesc.n_rows);
 
-    cout << query.rows <<" "<<query.cols << endl;
-
     cvflann::Matrix<int> indices(new int[query.rows*queryKnn], query.rows, queryKnn);
     cvflann::Matrix<double> dists(new double[query.rows*queryKnn], query.rows, queryKnn);
 
-    cout << "Load index file\n";
-    cvflann::Index<cvflann::L2<double> > treeIndex(dataset, cvflann::SavedIndexParams(indexFile));
-    cout << "Loaded sucessfully\n";
-
-    cout << "Start knn search\n";
-    treeIndex.knnSearch(query, indices, dists, queryKnn, cvflann::SearchParams(nChecks));
-    cout << "knn search sucessfully\n";
+    treeIndex->knnSearch(query, indices, dists, queryKnn, cvflann::SearchParams(nChecks));
 
     umat bins(queryKnn, query.rows);
     memcpy(bins.memptr(), indices.data, query.rows * queryKnn * sizeof(int));
