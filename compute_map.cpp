353f9d7bc17045496e73175ea34311501d07e012
Tmbao <tmbao@apcs.vn>
2015-06-03 22:51:19 +0700

    Fix indentation

--- compute_map.cpp
+++ compute_map.cpp
@@ -6,11 +6,15 @@ using namespace std;
 const string apFolder = "data/ap";
 
 int main() {
-	DIR *dir = opendir(apFolder.c_str());
-	double mAP = 0;
-	int cnt = 0;
+    DIR *dir = opendir(apFolder.c_str());
+    double mAP = 0;
+    int cnt = 0;
     while (dirent *pdir = readdir(dir)) {
         string fName = pdir->d_name;
+
+        if (fName == '..')
+            continue;
+
         ifstream fin(fName.c_str());
         double ap;
 
